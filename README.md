3d80a0d056c3e9c1cac136ddb642e09512f32833
agwebberley <agwebberley@archuser.fyi>
2022-09-22 15:38:51 -0700

    ALLOWED_HOSTS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Librehost-Blog
+
+Django Blog built as a project for a personal choice homework in Introduction To Computer Programming.
--- librehost/settings.py
+++ librehost/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3o(8%6$5yolax^h-3j#^benim8mfxipvj$rx#k2yt6qd=yhkh#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "librehost.io"]
 
 
 # Application definition

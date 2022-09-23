d104c95267b13884b049bd04b0d33a246dd6de4a
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2022-09-22 23:15:02 -0700

    Migration: Image Field Optional

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from django.db import models
 
 # Create your models here.
@@ -7,7 +8,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", blank=True)
 
     def __str__(self):
         return self.title
